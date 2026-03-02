a52bdcf7602ab15323362a5704713ead6db7f3bb
BashukOleksii <oleksiichortpovnii@gmail.com>
2026-03-02 23:29:56 +0200

    Add _ program.cs configuration to JWT Authorization

---
+++ InventoryManagementSystem/InventorySystem-API/User/Services/TokenGenerator.cs
@@ -0,0 +1,12 @@
+﻿using InventorySystem_API.User.Model;
+
+namespace InventorySystem_API.User.Services
+{
+    public class TokenGenerator
+    {
+        public string GenerateAccessToken(UserModel userModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
