c1cc4dbfc792fec4667c1361ec5b3aa9ee16ccce
BashukOleksii <oleksiichortpovnii@gmail.com>
2026-03-12 17:40:38 +0200

    Add _ company repository and interface

---
+++ InventoryManagementSystem/InventorySystem-API/Company/Repository/CompanyRepository.cs
@@ -0,0 +1,35 @@
+﻿using InventorySystem_API.Company.Models;
+using MongoDB.Driver;
+
+namespace InventorySystem_API.Company.Repository
+{
+    public class CompanyRepository : ICompanyRepository
+    {
+        private readonly IMongoCollection<CompanyModel> _collection;
+
+        public CompanyRepository(IMongoDatabase mongoDatabase) =>
+            _collection = mongoDatabase.GetCollection<CompanyModel>("Companies");
+
+        public async Task<CompanyModel> Create(CompanyModel companyModel) { 
+            await _collection.InsertOneAsync(companyModel);
+            return companyModel;
+        }
+
+
+        public async Task Delete(string id) =>
+            await _collection.DeleteOneAsync(company => company.Id == id);
+
+
+        public async Task<CompanyModel?> GetById(string id) =>
+            await _collection.Find(company => company.Id == id).FirstOrDefaultAsync();
+
+
+
+        public async Task<CompanyModel> Update(CompanyModel companyModel)
+        {
+            await _collection.ReplaceOneAsync(company => company.Id == companyModel.Id, companyModel);
+            return companyModel;
+        }
+
+    }
+}
---
+++ InventoryManagementSystem/InventorySystem-API/Company/Repository/ICompanyRepository.cs
@@ -0,0 +1,12 @@
+﻿using InventorySystem_API.Company.Models;
+
+namespace InventorySystem_API.Company.Repository
+{
+    public interface ICompanyRepository
+    {
+        Task<CompanyModel?> GetById(string id);
+        Task Delete(string id);
+        Task<CompanyModel> Create(CompanyModel companyModel);
+        Task<CompanyModel> Update(CompanyModel companyModel);
+    }
+}
