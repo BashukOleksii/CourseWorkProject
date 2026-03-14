ab1a05ebede49a5a5aba9974bec192352181c1bb
BashukOleksii <oleksiichortpovnii@gmail.com>
2026-03-14 11:42:17 +0200

    Add _ warehouse repository

---
+++ InventoryManagementSystem/InventorySystem-API/Warehouse/Repository/IWarehouseRepository.cs
@@ -0,0 +1,16 @@
+﻿using InventorySystem_API.Warehouse.Models;
+
+namespace InventorySystem_API.Warehouse.Repository
+{
+    public interface IWarehouseRepository
+    {
+        Task<WarehouseModel?> GeById(string id);
+        Task<List<WarehouseModel>?> GetByIds(string[] id);
+
+        Task<WarehouseModel> Create(WarehouseModel model);
+        Task<WarehouseModel> Update(WarehouseModel model);
+
+        Task Delete(string id);
+
+    }
+}
---
+++ InventoryManagementSystem/InventorySystem-API/Warehouse/Repository/WarehouseRepository.cs
@@ -0,0 +1,37 @@
+﻿using InventorySystem_API.Warehouse.Models;
+using MongoDB.Driver;
+
+namespace InventorySystem_API.Warehouse.Repository
+{
+    public class WarehouseRepository : IWarehouseRepository
+    {
+        private readonly IMongoCollection<WarehouseModel> _collection;
+
+        public WarehouseRepository(IMongoDatabase database) =>
+            _collection = database.GetCollection<WarehouseModel>("Warehouses");
+
+        public async Task<WarehouseModel> Create(WarehouseModel model)
+        {
+            await _collection.InsertOneAsync(model);
+            return model;
+        }
+
+        public async Task Delete(string id) =>
+            await _collection.DeleteOneAsync(warehouse => warehouse.Id == id);
+
+
+        public async Task<WarehouseModel?> GeById(string id) =>
+            await _collection.Find(warehouse => warehouse.Id == id).FirstOrDefaultAsync();
+
+
+        public async Task<List<WarehouseModel>?> GetByIds(string[] ids) =>
+            await _collection.Find(warehouse => ids.Contains(warehouse.Id)).ToListAsync();
+        
+
+        public async Task<WarehouseModel> Update(WarehouseModel model)
+        {
+            await _collection.ReplaceOneAsync(warehouse => warehouse.Id == model.Id,model);
+            return model;
+        }
+    }
+}
