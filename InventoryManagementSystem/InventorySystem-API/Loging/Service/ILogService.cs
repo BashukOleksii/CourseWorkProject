bdc844d1b883a689ce9639746d0c163630c6ab2a
BashukOleksii <oleksiichortpovnii@gmail.com>
2026-03-25 19:10:28 +0200

    Add . auditLog service

---
+++ InventoryManagementSystem/InventorySystem-API/Loging/Service/ILogService.cs
@@ -0,0 +1,14 @@
+﻿using InventorySystem_API.Loging.Models;
+using InventorySystem_Shared.Loging;
+using MongoDB.Driver;
+
+namespace InventorySystem_API.Loging.Service
+{
+    public interface ILogService
+    {
+        Task<List<AuditLogResponse>> Get(AuditLogQuery auditLogQuery, string companyId);
+        Task<AuditLogResponse> GetById(string id);
+        Task<AuditLogResponse> Create(AuditLogCreate dto);
+        Task Delete(string id);
+    }
+}
