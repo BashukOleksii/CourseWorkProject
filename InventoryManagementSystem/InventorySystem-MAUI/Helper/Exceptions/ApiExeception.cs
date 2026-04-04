137e5cbf03ef4b376eabedd1b1e74b6f4451ccd9
BashukOleksii <oleksiichortpovnii@gmail.com>
2026-04-04 20:20:05 +0300

    Add _ auth-service

---
+++ InventoryManagementSystem/InventorySystem-MAUI/Helper/Exceptions/ApiExeception.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InventorySystem_MAUI.Helper.Exceptions
+{
+    public class ApiException : Exception
+    {
+        public int StatusCode { get; }
+        public string Content { get; }
+
+        public ApiException(int statusCode, string content)
+        {
+            StatusCode = statusCode;
+            Content = content;
+        }
+
+
+        public static async Task ShowException(HttpResponseMessage? message)
+        {
+            var messageContent = await message.Content.ReadAsStringAsync();
+            throw new ApiException((int)message.StatusCode, messageContent);
+        }
+    }
+}
+
---
+++ InventoryManagementSystem/InventorySystem-MAUI/Service/AuthService.cs
@@ -0,0 +1,32 @@
+﻿using InventorySystem_MAUI.Helper;
+using InventorySystem_Shared.User;
+using System;
+using System.Collections.Generic;
+using System.Net.Http.Json;
+using System.Text;
+
+namespace InventorySystem_MAUI.Service
+{
+    public class AuthService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly UserContextService _userContextService;
+
+        public AuthService(HttpClient httpClient, UserContextService userContextService)
+        {
+            _httpClient = httpClient;
+            _userContextService = userContextService;
+        }
+
+        public async Task Login(string email, string password)
+        {
+            var response = await _httpClient.PostAsJsonAsync("auth/sign-in", new UserLogin { Email = email, Password = password });
+
+            if (!response.IsSuccessStatusCode)
+            {
+                var 
+            }
+
+        }
+    }
+}
