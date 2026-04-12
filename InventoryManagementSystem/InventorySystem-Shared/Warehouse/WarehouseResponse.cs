26412dfde3e107b128961484310321ae73f5a80f
BashukOleksii <oleksiichortpovnii@gmail.com>
2026-04-12 13:59:45 +0300

    Add some changes

--- InventoryManagementSystem/InventorySystem-Shared/Warehouse/WarehouseResponse.cs
+++ InventoryManagementSystem/InventorySystem-Shared/Warehouse/WarehouseResponse.cs
@@ -1,12 +1,17 @@
-﻿using System;
+﻿using InventorySystem_Shared.AddressClass;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace InventorySystem_Shared.Warehouse
 {
-    public class WarehouseResponse : WarehouseDTO
+    public class WarehouseResponse
     {
         public string Id { get; set; } = string.Empty;
         public string CompanyId { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public Address Address { get; set; }
+        public double Area { get; set; }
     }
 }
