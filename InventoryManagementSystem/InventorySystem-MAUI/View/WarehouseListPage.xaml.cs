daaf514d58c9ae42e431aa08819f0ff73988b279
BashukOleksii <oleksiichortpovnii@gmail.com>
2026-04-12 22:38:15 +0300

    Add some changes

--- InventoryManagementSystem/InventorySystem-MAUI/View/WarehouseListPage.xaml.cs
+++ InventoryManagementSystem/InventorySystem-MAUI/View/WarehouseListPage.xaml.cs
@@ -4,9 +4,19 @@ namespace InventorySystem_MAUI.View;
 
 public partial class WarehouseListPage : ContentPage
 {
-	public WarehouseListPage(WarehouseListViewModel warehouseListViewModel)
-	{
-		InitializeComponent();
-		BindingContext = warehouseListViewModel;
-	}
+    public WarehouseListPage(WarehouseListViewModel warehouseListViewModel)
+    {
+        InitializeComponent();
+        BindingContext = warehouseListViewModel;
+    }
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        var vm = BindingContext as WarehouseListViewModel;
+        if (vm != null)
+            await vm.LoadWarehouses();
+        
+
+    }
 }
--- InventoryManagementSystem/InventorySystem-MAUI/ViewModel/WarehouseDetailsViewModel.cs
+++ InventoryManagementSystem/InventorySystem-MAUI/ViewModel/WarehouseDetailsViewModel.cs
@@ -109,7 +109,7 @@ namespace InventorySystem_MAUI.ViewModel
         [RelayCommand]
         private async Task Cancel()
         {
-            await Shell.Current.GoToAsync("..");
+            await ShellService.GoBack();
         }
     }
 }
