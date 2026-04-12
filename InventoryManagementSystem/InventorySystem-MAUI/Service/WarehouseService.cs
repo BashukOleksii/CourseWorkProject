8658f19e5ce95b01bbd5f38739396d772847a4e8
BashukOleksii <oleksiichortpovnii@gmail.com>
2026-04-12 23:42:14 +0300

    Add _ method to warehouse service

--- InventoryManagementSystem/InventorySystem-MAUI/Service/WarehouseService.cs
+++ InventoryManagementSystem/InventorySystem-MAUI/Service/WarehouseService.cs
@@ -1,8 +1,5 @@
 ﻿using InventorySystem_Shared.Warehouse;
-using System;
-using System.Collections.Generic;
 using System.Net.Http.Json;
-using System.Text;
 
 namespace InventorySystem_MAUI.Service
 {
@@ -17,37 +14,7 @@ namespace InventorySystem_MAUI.Service
 
         public async Task<List<WarehouseResponse>> GetWarehouses(WarehouseQuery query)
         {
-            var queryString = $"?Page={query.Page}&PageSize={query.PageSize}";
-            
-            if (!string.IsNullOrEmpty(query.Name)) 
-                queryString += $"&Name={query.Name}";
-            if (!string.IsNullOrEmpty(query.PartDescription))
-                queryString += $"&PartDescription={query.PartDescription}";
-            if (query.Address is not null)
-            {
-                if(!string.IsNullOrEmpty(query.Address.Country))
-                    queryString += $"&Address.Country={query.Address.Country}";
-                if (!string.IsNullOrEmpty(query.Address.State))
-                    queryString += $"&Address.State={query.Address.State}";
-                if (!string.IsNullOrEmpty(query.Address.District))
-                    queryString += $"&Address.District={query.Address.District}";
-                if (!string.IsNullOrEmpty(query.Address.City))
-                    queryString += $"&Address.City={query.Address.City}";
-                if (!string.IsNullOrEmpty(query.Address.Street))
-                    queryString += $"&Address.Street={query.Address.Street}";
-                if (!string.IsNullOrEmpty(query.Address.HouseNumber))
-                    queryString += $"&Address.HouseNumber={query.Address.HouseNumber}";
-            }
-
-            if (query.MinArea.HasValue)
-                queryString += $"&MinArea={query.MinArea.Value}";
-            if (query.MaxArea.HasValue)
-                queryString += $"&MaxArea={query.MaxArea.Value}";
-
-            if(!string.IsNullOrEmpty(query.SortBy))
-                queryString += $"&SortBy={query.SortBy}&OrderByDescending={query.OrderByDescending}";
-
-            var response = await _httpClient.GetAsync($"api/warehouse{queryString}");
+            var response = await _httpClient.GetAsync($"api/warehouse{BuildQueryString(query)}");
 
             return await response.Content.ReadFromJsonAsync<List<WarehouseResponse>>();
         }
@@ -70,6 +37,47 @@ namespace InventorySystem_MAUI.Service
         {
             var response = await _httpClient.GetAsync($"api/warehouse/{id}");
             return await response.Content.ReadFromJsonAsync<WarehouseResponse>();
+        }
+
+        public async Task<byte[]> GetWarehouseReport(WarehouseQuery query)
+        {
+            var queryString = BuildQueryString(query);
+            var response = await _httpClient.GetAsync($"api/export/warehouse-report{queryString}");
+            return await response.Content.ReadAsByteArrayAsync();
+        }
+
+        private string BuildQueryString(WarehouseQuery query)
+        {
+            var queryString = $"?Page={query.Page}&PageSize={query.PageSize}";
+
+            if (!string.IsNullOrEmpty(query.Name))
+                queryString += $"&Name={query.Name}";
+            if (!string.IsNullOrEmpty(query.PartDescription))
+                queryString += $"&PartDescription={query.PartDescription}";
+            if (query.Address is not null)
+            {
+                if (!string.IsNullOrEmpty(query.Address.Country))
+                    queryString += $"&Address.Country={query.Address.Country}";
+                if (!string.IsNullOrEmpty(query.Address.State))
+                    queryString += $"&Address.State={query.Address.State}";
+                if (!string.IsNullOrEmpty(query.Address.District))
+                    queryString += $"&Address.District={query.Address.District}";
+                if (!string.IsNullOrEmpty(query.Address.City))
+                    queryString += $"&Address.City={query.Address.City}";
+                if (!string.IsNullOrEmpty(query.Address.Street))
+                    queryString += $"&Address.Street={query.Address.Street}";
+                if (!string.IsNullOrEmpty(query.Address.HouseNumber))
+                    queryString += $"&Address.HouseNumber={query.Address.HouseNumber}";
+            }
+
+            if (query.MinArea.HasValue)
+                queryString += $"&MinArea={query.MinArea.Value}";
+            if (query.MaxArea.HasValue)
+                queryString += $"&MaxArea={query.MaxArea.Value}";
+
+            if (!string.IsNullOrEmpty(query.SortBy))
+                queryString += $"&SortBy={query.SortBy}&OrderByDescending={query.OrderByDescending}";
+            return queryString;
         }
 
     }
