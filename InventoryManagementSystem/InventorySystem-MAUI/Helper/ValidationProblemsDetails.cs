02475f33f43963b74f63763ca19f4646806fb58e
BashukOleksii <oleksiichortpovnii@gmail.com>
2026-04-25 20:46:06 +0300

    Add _ parsing object response

---
+++ InventoryManagementSystem/InventorySystem-MAUI/Helper/ValidationProblemsDetails.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InventorySystem_MAUI.Helper
+{
+    public class ValidationProblemDetails
+    {
+        public string Title { get; set; }
+        public int Status { get; set; }
+        public Dictionary<string, string[]> Errors { get; set; }
+    }
+}
